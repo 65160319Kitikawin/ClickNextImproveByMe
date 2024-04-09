9326c5e00efebc4c534bfde25a2e60a508f2bb0c
Kitikawin Hompan <152171870+65160319Kitikawin@users.noreply.github.com>
2024-04-10 03:36:55 +0700

    test

--- storage/framework/views/c44be8d9bea284c07a6ee5c203ec1ae9.php
+++ storage/framework/views/c44be8d9bea284c07a6ee5c203ec1ae9.php
@@ -17,7 +17,7 @@
         integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>@yield('title')</title>
+    <title><?php echo $__env->yieldContent('title'); ?></title>
     <style>
         * {
             font-family: "Roboto", sans-serif;
@@ -489,28 +489,6 @@
             background-color: #086dd3;
             transition: all 0.3s;
         }
-        .comment_button {
-            width: 110px; 
-            height:30px; 
-            border-radius:5px 0px 0px 5px;
-        }
-
-        .comment_card {
-            z-index: 1; 
-            position: fixed; 
-            width: 300px; 
-            height: 100vh; 
-            right: 0;
-            top: 120.5px;
-        }
-
-        .comment_close_button {
-            position: fixed; 
-            right: 0;
-            border: none; 
-            background-color: white;
-        }
-
     </style>
 </head>
 
@@ -519,11 +497,11 @@
     <aside class="d-flex flex-shrink-0 flex-column primary"
         style="width:5.625rem; min-height: 100vh; overflow-y: auto;">
         <div class="list-group flex-column mb-auto">
-            <a href="{{ route('home.index') }}" style="height: 65px"
+            <a href="<?php echo e(route('home.index')); ?>" style="height: 65px"
                 class="text-decoration-none d-flex justify-content-center align-items-center">
-                <img width="50px" src="{{ url('/assets/icon/clicknext_logo.webp') }}" alt="clicknext-logo">
+                <img width="50px" src="<?php echo e(url('/assets/icon/clicknext_logo.webp')); ?>" alt="clicknext-logo">
             </a>
-            @yield('sidebar')
+            <?php echo $__env->yieldContent('sidebar'); ?>
         </div>
     </aside>
     <!-- End Sidebar -->
@@ -552,19 +530,19 @@
                         </div>
                         <div class="col-5 d-flex align-items-center p-1" style="height: 40px;">
                             <a class="m-auto rounded-1 w-100 btn btn-secondary pt-1"
-                                href="{{ route('workspace.create') }}">Create Workspace</a>
+                                href="<?php echo e(route('workspace.create')); ?>">Create Workspace</a>
                         </div>
                     </div>
                     <div class="row">
                         <label style="color: #808080; font-size: 14px; font-weight: 500;">Recently visited</label>
                         <ul style="list-style-type:none;">
-                            @foreach ($workspaces as $index => $workspace)
-                                @if ($index < 3)
+                            <?php $__currentLoopData = $workspaces; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $index => $workspace): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <?php if($index < 3): ?>
                                     <div class="row custom-table" style="border: none">
                                         <div class="col">
                                             <li class="d-flex align-items-center mt-1 link-black" style="height: 30px">
                                                 <a class="link-black" style="width: 100%; height:100%"
-                                                    href="{{ route('workspace.index', ['workspace' => $workspace->id]) }}">
+                                                    href="<?php echo e(route('workspace.index', ['workspace' => $workspace->id])); ?>">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                                                         class="me-2" viewBox="0 0 16 16" width="18"
                                                         height="18">
@@ -573,13 +551,13 @@
                                                         </path>
                                                     </svg>
                                                     <label label class="fs-6 fw-normal cursor"
-                                                        for="">{{ $workspace->name }}</label>
+                                                        for=""><?php echo e($workspace->name); ?></label>
                                                 </a>
                                             </li>
                                         </div>
                                     </div>
-                                @endif
-                            @endforeach
+                                <?php endif; ?>
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                         </ul>
                     </div>
                     <div class="row align-items-end mt-auto" style="border-top: #F2F2F2 solid 2px;">
@@ -595,30 +573,30 @@
                     data-bs-toggle="dropdown" aria-expanded="false">
                     <img src="https://media.discordapp.net/attachments/994685233087643719/1215271120127791114/77ed449a829d201a7940b0f98d49ca5a3cf43dd9.jpg?ex=65fc246d&is=65e9af6d&hm=cc53b20e7bac20faa1f57f479c85b3a5c19f166a5ece6b0da943736fc79cb017&=&format=webp"
                         alt="" width="40" height="40" class="rounded-circle me-2">
-                    <span class="fs-5 fw-normal">{{ Auth::user()->name }}</span>
+                    <span class="fs-5 fw-normal"><?php echo e(Auth::user()->name); ?></span>
                     <span id="dropdown-profile-icon" class="material-icons">expand_more</span>
                 </button>
                 <ul class="dropdown-menu pane" aria-labelledby="dropdownMenuButton1">
                     <li><a class="dropdown-item" href="#">View Profile</a></li>
                     <li>
-                        <form action="{{ route('logout') }}" method="POST">
-                            @csrf
-                            @method('DELETE')
+                        <form action="<?php echo e(route('logout')); ?>" method="POST">
+                            <?php echo csrf_field(); ?>
+                            <?php echo method_field('DELETE'); ?>
                             <button class="dropdown-item" type="submit">Sign Out</button>
                         </form>
                     </li>
                 </ul>
             </div>
         </nav>
-        @if ($message = Session::get('success'))
-            <div class="alert alert-success">{{ $message }}</div>
-        @endif
+        <?php if($message = Session::get('success')): ?>
+            <div class="alert alert-success"><?php echo e($message); ?></div>
+        <?php endif; ?>
         <!-- Start Content -->
-        @yield('content')
+        <?php echo $__env->yieldContent('content'); ?>
         <!-- End Content -->
     </div>
     <!-- End Main -->
-    @yield('js')
+    <?php echo $__env->yieldContent('js'); ?>
     <script>
         (() => {
             'use strict'
@@ -666,3 +644,4 @@
 </body>
 
 </html>
+<?php /**PATH C:\Users\MrLucky\Desktop\Try\OSSDProject\OSSD_Realv2\resources\views/layouts/default.blade.php ENDPATH**/ ?>
--- storage/framework/views/f892a0f2a867ac726a31ab18f108c485.php
+++ storage/framework/views/f892a0f2a867ac726a31ab18f108c485.php
@@ -1,29 +1,27 @@
-@extends('layouts.default')
+<?php $__env->startSection('title', 'Collection'); ?>
 
-@section('title', 'Collection')
-
-@section('sidebar')
-    <a href="{{ route('workspace.collections', ['workspace' => $selectedWorkspace->id]) }}"
-        class="list-group-items hover-white btn-menu @if (request()->routeIs('workspace.collections')) focus @endif"
+<?php $__env->startSection('sidebar'); ?>
+    <a href="<?php echo e(route('workspace.collections', ['workspace' => $selectedWorkspace->id])); ?>"
+        class="list-group-items hover-white btn-menu <?php if(request()->routeIs('workspace.collections')): ?> focus <?php endif; ?>"
         style="text-decoration: none;">
         <span class="material-symbols-outlined" style="font-size: 36px;">folder</span>
         <label class="fw-normal cursor" style="color: white" for="">Collections</label>
     </a>
-    <a href="{{ route('workspace.history', ['workspace' => $selectedWorkspace->id]) }}"
-        class="list-group-items hover-white  btn-menu @if (request()->routeIs('workspace.history')) focus @endif"
+    <a href="<?php echo e(route('workspace.history', ['workspace' => $selectedWorkspace->id])); ?>"
+        class="list-group-items hover-white  btn-menu <?php if(request()->routeIs('workspace.history')): ?> focus <?php endif; ?>"
         style="text-decoration: none;">
         <span class="material-symbols-outlined" style="font-size: 36px;">manage_history</span>
         <label class="fw-normal cursor" style="color: white" for="">History</label>
     </a>
-    <a href="{{ route('workspace.trash', ['workspace' => $selectedWorkspace->id]) }}"
-        class="list-group-items hover-white  btn-menu @if (request()->routeIs('workspace.trash')) focus @endif"
+    <a href="<?php echo e(route('workspace.trash', ['workspace' => $selectedWorkspace->id])); ?>"
+        class="list-group-items hover-white  btn-menu <?php if(request()->routeIs('workspace.trash')): ?> focus <?php endif; ?>"
         style="text-decoration: none;">
         <span class="material-symbols-outlined" style="font-size: 36px;">delete</span>
         <label class="fw-normal cursor" style="color: white" for="">Trash</label>
     </a>
-@endsection
+<?php $__env->stopSection(); ?>
 
-@section('content')
+<?php $__env->startSection('content'); ?>
     <section class="content d-flex flex-row" style="min-height: 1200px; overflow-y: auto;">
         <!-- Collection list -->
         <div style="width: 280px; border-right: #f2f2f2 solid 1px;">
@@ -35,7 +33,7 @@
                         d="M10.561 8.073a6.005 6.005 0 0 1 3.432 5.142.75.75 0 1 1-1.498.07 4.5 4.5 0 0 0-8.99 0 .75.75 0 0 1-1.498-.07 6.004 6.004 0 0 1 3.431-5.142 3.999 3.999 0 1 1 5.123 0ZM10.5 5a2.5 2.5 0 1 0-5 0 2.5 2.5 0 0 0 5 0Z">
                     </path>
                 </svg>
-                <p class="mt-3 ms-2">{{ $selectedWorkspace->name }}</p>
+                <p class="mt-3 ms-2"><?php echo e($selectedWorkspace->name); ?></p>
                 <div class="dropdown p-0 ms-4">
                     <button class="btn" type="button" id="workspace_option" data-bs-toggle="dropdown"
                         aria-expanded="false">
@@ -43,10 +41,10 @@
                     </button>
                     <ul class="dropdown-menu pane" aria-labelledby="workspace_option">
                         <li><a class="dropdown-item "
-                                href="{{ route('workspace.setting', ['workspace' => $selectedWorkspace->id]) }}">Setting</a>
+                                href="<?php echo e(route('workspace.setting', ['workspace' => $selectedWorkspace->id])); ?>">Setting</a>
                         </li>
                         <li><a class="dropdown-item"
-                                href="{{ route('workspace.deleteWorkspace', ['workspace' => $selectedWorkspace->id]) }}">Delete</a>
+                                href="<?php echo e(route('workspace.deleteWorkspace', ['workspace' => $selectedWorkspace->id])); ?>">Delete</a>
                         </li>
                     </ul>
                 </div>
@@ -61,26 +59,35 @@
                     </div>
                 </div>
                 <!-- Collections List -->
-                @foreach ($selectedWorkspace->collections as $collection)
+                <?php $__currentLoopData = $selectedWorkspace->collections; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $collection): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                     <div class="row">
                         <div class="col p-0">
                             <button class="btn-collapse dropdown hover-black d-flex align-items-center"
                                 style="height: 30px; width: 100%; text-decoration:none;" type="button"
-                                data-bs-toggle="collapse" data-bs-target="#collection_{{ $collection->id }}"
-                                aria-expanded="false" aria-controls="collection_{{ $collection->id }}">
+                                data-bs-toggle="collapse" data-bs-target="#collection_<?php echo e($collection->id); ?>"
+                                aria-expanded="false" aria-controls="collection_<?php echo e($collection->id); ?>">
                                 <span class="material-symbols-outlined ms-1 me-2" name="expand"
-                                    id="{{ $collection->id }}">chevron_right</span>
-                                <span class="fs-6" style="font-weight: 500">{{ $collection->name }}</span>
+                                    id="<?php echo e($collection->id); ?>">chevron_right</span>
+                                <span class="fs-6" style="font-weight: 500"><?php echo e($collection->name); ?></span>
                             </button>
-                            <div class="collapse" id="collection_{{ $collection->id }}"
-                                @if (session()->has('collection_' . $collection->id . '_collapse') &&
-                                        session('collection_' . $collection->id . '_collapse')) aria-expanded="true" @endif>
-                                {{-- Method List --}}
+                            <div class="collapse" id="collection_<?php echo e($collection->id); ?>"
+                                <?php if(session()->has('collection_' . $collection->id . '_collapse') &&
+                                        session('collection_' . $collection->id . '_collapse')): ?> aria-expanded="true" <?php endif; ?>>
                                 
+                                <?php $__currentLoopData = $collection->methods; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $method): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <ul class="navbar-nav">
+                                        <li><a
+                                                href="<?php echo e(route('workspace.editCollection', ['workspace' => $selectedWorkspace->id, 'collection' => $collection->id])); ?>"><label
+                                                    class="me-2" style="font-size: 14px;  font-weight: 500;"
+                                                    for=""><?php echo e($method->type); ?></label><label for=""
+                                                    style="font-size: 14px; color: #000;"><?php echo e($method->path); ?></label></a>
+                                        </li>
+                                    </ul>
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                             </div>
                         </div>
                     </div>
-                @endforeach
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
             </div>
         </div>
@@ -89,35 +96,36 @@
         <div class="" style="width: 100%;">
             <!-- Nav Tabs -->
             <ul class="nav nav-tabs flex-row" role="tablist" style="height: 55px;">
-                @php
+                <?php
                     $collection_tabs = session('collection_tabs', []);
-                @endphp
-                @foreach (array_reverse($collection_tabs) as $collection)
+                ?>
+                <?php $__currentLoopData = array_reverse($collection_tabs); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $collection): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                     <li class="nav-items">
-                        <button class="nav-link fst-italic @if (request()->routeIs('workspace.editCollection') && request('collection') == $collection->id) active @endif"
-                            onclick="window.location='{{ route('workspace.editCollection', ['workspace' => $selectedWorkspace->id, 'collection' => $collection->id]) }}'"
-                            role="tab" id="view_{{ $collection->id }}" data-bs-toggle="tab">
-                            {{ $collection->name }}
+                        <button class="nav-link fst-italic <?php if(request()->routeIs('workspace.editCollection') && request('collection') == $collection->id): ?> active <?php endif; ?>"
+                            onclick="window.location='<?php echo e(route('workspace.editCollection', ['workspace' => $selectedWorkspace->id, 'collection' => $collection->id])); ?>'"
+                            role="tab" id="view_<?php echo e($collection->id); ?>" data-bs-toggle="tab">
+                            <?php echo e($collection->name); ?>
+
                             <a class="btn fs-5 p-0 material-symbols-outlined"
-                                href='{{ route('delete.collection.tabs', ['workspace' => $selectedWorkspace->id, 'collection' => $collection->id]) }}'">close</a>
+                                href='<?php echo e(route('delete.collection.tabs', ['workspace' => $selectedWorkspace->id, 'collection' => $collection->id])); ?>'">close</a>
                         </button>
                     </li>
-                @endforeach
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                 <a style="text-decoration: none"
-                    href="{{ route('workspace.editCollection', ['workspace' => $selectedWorkspace->id, 'collection' => -1]) }}"
+                    href="<?php echo e(route('workspace.editCollection', ['workspace' => $selectedWorkspace->id, 'collection' => -1])); ?>"
                     class="d-flex justify-content-center align-items-center p-2 add-nav-items">
                     <span class="material-symbols-outlined">add</span>
                 </a>
             </ul>
             <!-- Tabs Content -->
             <div class="tab-content">
-                @yield('collection_template')
+                <?php echo $__env->yieldContent('collection_template'); ?>
             </div>
         </div>
     </section>
-@endsection
+<?php $__env->stopSection(); ?>
 
-@section('js')
+<?php $__env->startSection('js'); ?>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             const collapseList = document.querySelectorAll('.collapse');
@@ -160,4 +168,6 @@
             });
         });
     </script>
-@endsection
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\MrLucky\Desktop\Try\OSSDProject\OSSD_Realv2\resources\views\collection.blade.php ENDPATH**/ ?>
